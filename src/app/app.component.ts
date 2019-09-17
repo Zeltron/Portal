260f9bd7262f1a5a199766e232b70124a0d6ddd2
Rahim <abdulrahim0704@gmail.com>
2019-09-17 12:44:51 +0800

    add project folder

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from './user.model';
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  	
+  user$: User[];
+
+  constructor(private dataService: DataService){
+
+  }
+  ngOnInit(){
+  	return this.dataService.getUsers()
+  	.subscribe(data => this.user$ = data);
+  }
+
+
+
+
+  
+}
